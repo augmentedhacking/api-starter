28441e48b24fc7ffb6d96057083c53e09956cb30
nien <nien@linebreak.studio>
2023-10-12 09:43:38 -0400

    Update comments.

--- APIStarter/ContentView.swift
+++ APIStarter/ContentView.swift
@@ -3,6 +3,7 @@
 //  APIStarter
 //
 //  Created by Nien Lam on 10/12/23.
+//  Copyright © 2023 Line Break, LLC. All rights reserved.
 //
 
 import SwiftUI
@@ -12,6 +13,7 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
+            // Access date string in view model.
             Text(viewModel.dateString)
                 .font(.title)
         }
--- APIStarter/ViewModel.swift
+++ APIStarter/ViewModel.swift
@@ -3,6 +3,7 @@
 //  APIStarter
 //
 //  Created by Nien Lam on 10/12/23.
+//  Copyright © 2023 Line Break, LLC. All rights reserved.
 //
 
 import Foundation
@@ -10,18 +11,18 @@ import Foundation
 @Observable
 class ViewModel {
     var dateString: String = ""
-  
     var timer: Timer?
     
-    
     init() {
+        // Setup timer.
         timer = Timer.scheduledTimer(timeInterval: 1,
                                      target: self,
                                      selector: #selector(updateTimer),
                                      userInfo: nil, 
                                      repeats: true)
     }
-    
+
+    // Helper method for getting current time.
     func getDateString() -> String {
         let formatter = DateFormatter()
         formatter.timeStyle = .long
